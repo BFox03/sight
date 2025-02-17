4c22fb8fd3f8096a7e02addfbc31752dd55fe479
Brandon Fox <brandonfox37@gmail.com>
2025-02-17 16:52:29 -0500

    Pivoted Code, Working minimum recognizing face and recording audio, removed Webserver aspect

--- FacialRecognition/FacialRecognition.ino
+++ FacialRecognition/FacialRecognition.ino
@@ -1,171 +1,168 @@
-#include "esp_camera.h"
-#include <WiFi.h>
-#include "ESP_I2S.h"
-#include "env.h"
+/**
+ * ESP32S3 Face Recognition
+ * (not detection!)
+ *
+ * Enroll a couple of faces 2-3 times each,
+ * then watch the ESP32 camera recognize between the two!
+ *
+ * Only works on ESP32 S3 chip.
+ */
 
-//
-// This project comes from [Espressif's routine CameraWebServer](https://github.com/espressif/arduino-esp32/blob/master/libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino) and has been changed to work directly with XIAO ESP32S3 Sense.
-//
+ // https://github.com/eloquentarduino/EloquentEsp32cam/tree/main
+#include <eloquent_esp32cam.h>
+#include <eloquent_esp32cam/face/detection.h>
+#include <eloquent_esp32cam/face/recognition.h>
 
-//
-// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
-//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
-//            Partial images will be transmitted if image exceeds buffer size
-//
-//            You must select partition scheme from the board menu that has at least 3MB APP space.
-//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15
-//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
+#include <I2S.h>
+#include "audio.h"
 
-// ===================
-// Select camera model
-// ===================
 
-/*
-#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-#include "camera_pins.h"
-*/
+using eloq::camera;
+using eloq::face::detection;
+using eloq::face::recognition;
 
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM     10
-#define SIOD_GPIO_NUM     40
-#define SIOC_GPIO_NUM     39
 
-#define Y9_GPIO_NUM       48
-#define Y8_GPIO_NUM       11
-#define Y7_GPIO_NUM       12
-#define Y6_GPIO_NUM       14
-#define Y5_GPIO_NUM       16
-#define Y4_GPIO_NUM       18
-#define Y3_GPIO_NUM       17
-#define Y2_GPIO_NUM       15
-#define VSYNC_GPIO_NUM    38
-#define HREF_GPIO_NUM     47
-#define PCLK_GPIO_NUM     13
+String prompt(String message);
 
-#define LED_GPIO_NUM      21
 
-//ADD
-#define BUTTON            D0
 
-// ===========================
-// Enter your WiFi credentials
-// ===========================
-const char *ssid = ssid_env;
-const char *password = password_env;
-
-void startCameraServer();
-void setupLedFlash(int pin);
-
+/**
+ *
+ */
 void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
+    delay(2000);
+    Serial.begin(115200);
+    Serial.println("Begin");
+    
+    pinMode(D0, INPUT);
 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  //config.pixel_format = PIXFORMAT_JPEG;  // for streaming
-  config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition *****************
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
+    I2S.setAllPins(-1, 42, 41, -1, -1);
+    if (!I2S.begin(PDM_MONO_MODE, SAMPLE_RATE, SAMPLE_BITS)) {
+      Serial.println("Failed to initialize I2S!");
+      while (1) ;
     }
-  } else {
-    // Best option for face detection/recognition                     ***********************
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
+    if(!SD.begin(21)){
+      Serial.println("Failed to mount SD Card!");
+      while (1) ;
+    }
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
+    // !!!!REPLACE WITH YOUR OWN MODEL!!!!
+    camera.pinout.xiao(); // e.g. xiao(), lilygo_tcamera_s3(), ...
+    camera.brownout.disable();
+    // face recognition only works at 240x240
+    camera.resolution.face();
+    camera.quality.high();
 
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
+    // face recognition only works with accurate detection
+    detection.accurate();
+    detection.confidence(0.6);
 
-  sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the brightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
+    // face recognition confidence
+    recognition.confidence(0.90);
 
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
+    // init camera
+    while (!camera.begin().isOk())
+        Serial.println(camera.exception.toString());
+    // init recognizer
+    while (!recognition.begin().isOk())
+        Serial.println(recognition.exception.toString());
 
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
+    Serial.println("Camera OK");
+    Serial.println("Face recognizer OK");
 
-// Setup LED FLash if LED pin is defined in camera_pins.h
-#if defined(LED_GPIO_NUM)
-  setupLedFlash(LED_GPIO_NUM);
-#endif
+    // delete stored data, if user confirms
+    if (prompt("Do you want to delete all existing faces? [yes|no]").startsWith("y")) {
+        Serial.println("Deleting all existing faces...");
+        recognition.deleteAll();
+    }
 
-  WiFi.begin(ssid, password);
-  WiFi.setSleep(false);
+    // dump stored faces, if user confirms
+    if (prompt("Do you want to dump existing faces? [yes|no]").startsWith("y")) {
+        recognition.dump();
+    }
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
+    Serial.println("Awaiting for face...");
 }
 
+
+/**
+ *
+ */
 void loop() {
-  // Do nothing. Everything is done in another task by the web server
-  delay(10000);
+    // capture picture
+    if (!camera.capture().isOk()) {
+        Serial.println(camera.exception.toString());
+        return;
+    }
+
+    // run face detection (not recognition!)
+    if (!recognition.detect().isOk())
+        return;
+
+    // if face is found, ask user to enroll or recognize
+    int reading = digitalRead(D0);
+    Serial.println(reading);
+
+    if (reading == 0) {
+        record_wav();
+        enroll();
+    }
+    else
+        recognize();
+
+    Serial.println("Awaiting for face...");
+}
+
+
+/**
+ * Ask user for input
+ */
+String prompt(String message) {
+    String answer;
+
+    do {
+        Serial.print(message);
+
+        while (!Serial.available())
+            delay(1);
+
+        answer = Serial.readStringUntil('\n');
+        answer.trim();
+    } while (!answer);
+
+    Serial.print(" ");
+    Serial.println(answer);
+    return answer;
+}
+
+
+/**
+ * Enroll new person
+ */
+void enroll() {
+    String name = prompt("Enter person name:");
+
+    if (recognition.enroll(name).isOk())
+        Serial.println("Success!");
+    else
+        Serial.println(recognition.exception.toString());
+}
+
+
+/**
+ * Recognize current face
+ */
+void recognize() {
+    if (!recognition.recognize().isOk()) {
+        Serial.println(recognition.exception.toString());
+        return;
+    }
+
+    Serial.print("Recognized face as ");
+    Serial.print(recognition.match.name.c_str());
+    Serial.print(" with confidence ");
+    Serial.print(recognition.match.similarity);
+    Serial.print(" (");
+    Serial.print(recognition.benchmark.millis());
+    Serial.println("ms)");
 }
--- FacialRecognition/env.h
+++
@@ -1,2 +0,0 @@
-const char *ssid_env = "Stevens-IoT";
-const char *password_env = "sSr373EWC6";
